0338e9b40ce94e4bffe4ad407f606d6fe4d55307
Suraj Baraili <baraileesuraj@gmail.com>
2021-04-27 20:58:34 +0545

    added instagram logo

--- src/NavBar/NavBar.js
+++ src/NavBar/NavBar.js
@@ -1,13 +1,20 @@
-const navWrapper={
-    color:'red',
-    borderBottom:'1px solid black',
-}
-
+import './NavBar.css'
+import Logo from  '../resources/insta_logo.png'
 const NavBar=()=>{
     return (
-    <div className={navWrapper}>
-            NavBar
+    <div className="navWrapper">
+        <div className="contentWrapper">
+            <div className="navLogo">
+                <a href='/' tabIndex="0">
+                    <div classname="">
+                        <img alt="Instagram" src={Logo}/>
+                    </div>
+                </a>
+            </div>
+            <div>asdasd</div>
+        </div>      
     </div>
     )
 }
 export default NavBar;
+
