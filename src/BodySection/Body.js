f3e7b5e9334a35bffce46b41b57778c0bec8b5ba
Suraj Baraili <baraileesuraj@gmail.com>
2021-05-12 19:52:01 +0545

    added basic page setup for body part of the site

---
+++ src/BodySection/Body.js
@@ -0,0 +1,13 @@
+import './Body.css'
+const Body=()=>{
+    return(
+            <div className="bodyWrapper">
+                <div className="bodyContent">
+                    asdasd
+                </div>
+            </div>    
+    
+    )
+}
+
+export default Body;
