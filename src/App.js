637e85fbda33c80ca8adcb946376bb6adc00f9d7
Suraj Baraili <baraileesuraj@gmail.com>
2021-06-01 20:37:19 +0545

    added react router and initial commit for explore section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import './App.css';
 import NavBar from './NavBar/NavBar'
 import Body from './BodySection/Body'
+import Explore from './Explore/explore'
+
+import {Route,Redirect,BrowserRouter as Router} from 'react-router-dom'
 function App() {
   return (
     <div>
-          <NavBar/>
-          <Body/>
+          <Router>
+            <NavBar></NavBar>
+                <Route exact path="/" component={Body} />
+                <Route exact path="/home">
+                  <Redirect to="/" />
+                </Route>
+                <Route exact path="/explore/" component={Explore} />
+                <Route exact path="/" component={Body} />
+                {/* <Route exact path="/About" component={About} /> */}
+          </Router>
     </div>
 
   );
