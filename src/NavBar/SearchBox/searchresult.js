8af8acb0f13eefdc1a2935149e79e729d6fccd71
Suraj Baraili <baraileesuraj@gmail.com>
2021-07-05 22:03:13 +0545

    worked on search result box

---
+++ src/NavBar/SearchBox/searchresult.js
@@ -0,0 +1,16 @@
+
+import './Searchresult.css'
+const searchresult=()=>{
+    return (
+        <div className='searchResult'>
+            <div className="diamondDiv">
+                
+            </div>
+            <div>
+                
+            </div>
+        </div>
+    )
+}
+
+export default searchresult;
