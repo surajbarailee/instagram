3c41530fb7c8ea1fa5519ddee08ed01a3e042cac
Suraj Baraili <baraileesuraj@gmail.com>
2021-07-29 22:19:08 +0545

    newsfeed work in progress

--- src/BodySection/Body.js
+++ src/BodySection/Body.js
@@ -1,7 +1,7 @@
 import './Body.css'
 import Story from './StorySection/Story'
 import ThreeDotMenu from '../resources/postNavMenu.svg'
-import PostImage from '../resources/posts/post1.jpg'
+
 import PlayButton from '../resources/play.png'
 
 
@@ -66,6 +66,7 @@ const NewsFeed = ()=>{
 
 
 const Post=(props)=>{
+    
     const [isPlaying,setIsPlaying] = useState(false)   
     const videoRef = useRef(null);
     const onPlay=()=>{
@@ -83,10 +84,10 @@ const Post=(props)=>{
             videoRef.current.pause();
         }
     }
-    if (props.video==='true'){
+    if (props.data[0]['type']==='video'){
         return(
             <div className="videoWrapper">
-                <video src={VideoPost}  
+                <video src={props.data[0]['mainpost']} 
                 onPlay={onPlay}
                 onPause={onPause}
                 style={{width: '100%'}} 
@@ -94,7 +95,6 @@ const Post=(props)=>{
                 <div className="controlsWrapper" onClick={playorPause}>
                 <img src={PlayButton} alt="" 
                 className={`controls ${isPlaying ? "controlHidden":"controlShown"} `}
-    
                 />
 
                 </div>
@@ -102,7 +102,7 @@ const Post=(props)=>{
         )
     }
     return(
-        <img src={PostImage} alt="post by memenepal" className="postImage" />
+        <img src={props.data[0]['mainpost']} alt={`post by${props.user}`} className="postImage" />
     )
 }
 
@@ -128,7 +128,7 @@ const SinglePost=(props)=>{
                         </div>
                     </div>
                     <div className="postBody" >
-                        <Post video={props.video} src={props.videosource}  alt="post by memenepal"/>
+                        <Post   alt="post by memenepal" data = {props.data['post']} user = {props.data['postowner']}/>
                     </div>
                     <div className="postNav" style={{paddingLeft:'0px',paddingTop:'12px'}}>
                         <div className="postNavLeft" >
@@ -136,7 +136,7 @@ const SinglePost=(props)=>{
                                     <svg aria-label="Like" fill="#262626" height="24" viewBox="0 0 48 48" width="24"><path d="M34.6 6.1c5.7 0 10.4 5.2 10.4 11.5 0 6.8-5.9 11-11.5 16S25 41.3 24 41.9c-1.1-.7-4.7-4-9.5-8.3-5.7-5-11.5-9.2-11.5-16C3 11.3 7.7 6.1 13.4 6.1c4.2 0 6.5 2 8.1 4.3 1.9 2.6 2.2 3.9 2.5 3.9.3 0 .6-1.3 2.5-3.9 1.6-2.3 3.9-4.3 8.1-4.3m0-3c-4.5 0-7.9 1.8-10.6 5.6-2.7-3.7-6.1-5.5-10.6-5.5C6 3.1 0 9.6 0 17.6c0 7.3 5.4 12 10.6 16.5.6.5 1.3 1.1 1.9 1.7l2.3 2c4.4 3.9 6.6 5.9 7.6 6.5.5.3 1.1.5 1.6.5.6 0 1.1-.2 1.6-.5 1-.6 2.8-2.2 7.8-6.8l2-1.8c.7-.6 1.3-1.2 2-1.7C42.7 29.6 48 25 48 17.6c0-8-6-14.5-13.4-14.5z"></path></svg>
                                 </div>
                                 <div className="actionIcons">
-                                    <svg aria-label="Comment" fill="#262626" height="24" viewBox="0 0 48 48" width="24"><path clipRule="evenodd" d="M47.5 46.1l-2.8-11c1.8-3.3 2.8-7.1 2.8-11.1C47.5 11 37 .5 24 .5S.5 11 .5 24 11 47.5 24 47.5c4 0 7.8-1 11.1-2.8l11 2.8c.8.2 1.6-.6 1.4-1.4zm-3-22.1c0 4-1 7-2.6 10-.2.4-.3.9-.2 1.4l2.1 8.4-8.3-2.1c-.5-.1-1-.1-1.4.2-1.8 1-5.2 2.6-10 2.6-11.4 0-20.6-9.2-20.6-20.5S12.7 3.5 24 3.5 44.5 12.7 44.5 24z" fill-rule="evenodd"></path></svg>                        </div>
+                                    <svg aria-label="Comment" fill="#262626" height="24" viewBox="0 0 48 48" width="24"><path clipRule="evenodd" d="M47.5 46.1l-2.8-11c1.8-3.3 2.8-7.1 2.8-11.1C47.5 11 37 .5 24 .5S.5 11 .5 24 11 47.5 24 47.5c4 0 7.8-1 11.1-2.8l11 2.8c.8.2 1.6-.6 1.4-1.4zm-3-22.1c0 4-1 7-2.6 10-.2.4-.3.9-.2 1.4l2.1 8.4-8.3-2.1c-.5-.1-1-.1-1.4.2-1.8 1-5.2 2.6-10 2.6-11.4 0-20.6-9.2-20.6-20.5S12.7 3.5 24 3.5 44.5 12.7 44.5 24z" fillRule="evenodd"></path></svg>                        </div>
                                 <div className="actionIcons">
                                     <svg aria-label="Share Post" fill="#262626" height="24" viewBox="0 0 48 48" width="24"><path d="M47.8 3.8c-.3-.5-.8-.8-1.3-.8h-45C.9 3.1.3 3.5.1 4S0 5.2.4 5.7l15.9 15.6 5.5 22.6c.1.6.6 1 1.2 1.1h.2c.5 0 1-.3 1.3-.7l23.2-39c.4-.4.4-1 .1-1.5zM5.2 6.1h35.5L18 18.7 5.2 6.1zm18.7 33.6l-4.4-18.4L42.4 8.6 23.9 39.7z"></path></svg>
                                 </div>
@@ -148,32 +148,30 @@ const SinglePost=(props)=>{
     
                     </div>
                     <div className="commentsWrapper">
+                        {props.data['likedby'].length>0 &&
                         <div className="likeStatus">
-                            Liked by <b>nanapatekar</b> and <b>others</b>
+                            Liked by <b>{props.data['likedby'][0]}</b> {props.data['likedby'].length>1 &&<span>and <b>others</b></span>}
                         </div>
+}
                         <div className="postDetails">
                             <b>{props.data.postowner}</b> {props.data.postCaption} 
                         </div>
                         <div className="commentStatus">
-                            View all 300 comments
+                            View all {Math.floor(Math.random() * 100)} comments
                         </div>
                         <div className="commentList">
-                            <div className="singleComment">
-                                <div className="usernameComment">
-                                    <b>addonnishedon</b> Can't wait!
+                                {props.data.comments.map((data,index)=>{
+                                    return <div className="singleComment" key={data.id}>
+                                    <div className="usernameComment">
+                                        <b>{data.username}</b> {data.comment}
+                                    </div>
+                                    <div className="commentReaction">
+                                        <svg aria-label="Like" fill="#262626" height="12" viewBox="0 0 48 48" width="12"><path d="M34.6 6.1c5.7 0 10.4 5.2 10.4 11.5 0 6.8-5.9 11-11.5 16S25 41.3 24 41.9c-1.1-.7-4.7-4-9.5-8.3-5.7-5-11.5-9.2-11.5-16C3 11.3 7.7 6.1 13.4 6.1c4.2 0 6.5 2 8.1 4.3 1.9 2.6 2.2 3.9 2.5 3.9.3 0 .6-1.3 2.5-3.9 1.6-2.3 3.9-4.3 8.1-4.3m0-3c-4.5 0-7.9 1.8-10.6 5.6-2.7-3.7-6.1-5.5-10.6-5.5C6 3.1 0 9.6 0 17.6c0 7.3 5.4 12 10.6 16.5.6.5 1.3 1.1 1.9 1.7l2.3 2c4.4 3.9 6.6 5.9 7.6 6.5.5.3 1.1.5 1.6.5.6 0 1.1-.2 1.6-.5 1-.6 2.8-2.2 7.8-6.8l2-1.8c.7-.6 1.3-1.2 2-1.7C42.7 29.6 48 25 48 17.6c0-8-6-14.5-13.4-14.5z"></path></svg>
+                                    </div>
                                 </div>
-                                <div className="commentReaction">
-                                    <svg aria-label="Like" fill="#262626" height="12" viewBox="0 0 48 48" width="12"><path d="M34.6 6.1c5.7 0 10.4 5.2 10.4 11.5 0 6.8-5.9 11-11.5 16S25 41.3 24 41.9c-1.1-.7-4.7-4-9.5-8.3-5.7-5-11.5-9.2-11.5-16C3 11.3 7.7 6.1 13.4 6.1c4.2 0 6.5 2 8.1 4.3 1.9 2.6 2.2 3.9 2.5 3.9.3 0 .6-1.3 2.5-3.9 1.6-2.3 3.9-4.3 8.1-4.3m0-3c-4.5 0-7.9 1.8-10.6 5.6-2.7-3.7-6.1-5.5-10.6-5.5C6 3.1 0 9.6 0 17.6c0 7.3 5.4 12 10.6 16.5.6.5 1.3 1.1 1.9 1.7l2.3 2c4.4 3.9 6.6 5.9 7.6 6.5.5.3 1.1.5 1.6.5.6 0 1.1-.2 1.6-.5 1-.6 2.8-2.2 7.8-6.8l2-1.8c.7-.6 1.3-1.2 2-1.7C42.7 29.6 48 25 48 17.6c0-8-6-14.5-13.4-14.5z"></path></svg>
-                                </div>
-                            </div>
-                            <div className="singleComment">
-                                <div className="usernameComment">
-                                    <b>addonnishedon</b> MY first comment YAY!!!
-                                </div>
-                                <div className="commentReaction">
-                                    <svg aria-label="Like" fill="#262626" height="12" viewBox="0 0 48 48" width="12"><path d="M34.6 6.1c5.7 0 10.4 5.2 10.4 11.5 0 6.8-5.9 11-11.5 16S25 41.3 24 41.9c-1.1-.7-4.7-4-9.5-8.3-5.7-5-11.5-9.2-11.5-16C3 11.3 7.7 6.1 13.4 6.1c4.2 0 6.5 2 8.1 4.3 1.9 2.6 2.2 3.9 2.5 3.9.3 0 .6-1.3 2.5-3.9 1.6-2.3 3.9-4.3 8.1-4.3m0-3c-4.5 0-7.9 1.8-10.6 5.6-2.7-3.7-6.1-5.5-10.6-5.5C6 3.1 0 9.6 0 17.6c0 7.3 5.4 12 10.6 16.5.6.5 1.3 1.1 1.9 1.7l2.3 2c4.4 3.9 6.6 5.9 7.6 6.5.5.3 1.1.5 1.6.5.6 0 1.1-.2 1.6-.5 1-.6 2.8-2.2 7.8-6.8l2-1.8c.7-.6 1.3-1.2 2-1.7C42.7 29.6 48 25 48 17.6c0-8-6-14.5-13.4-14.5z"></path></svg>
-                                </div>
-                            </div>
+                                })
+                                    
+                            }
                         
                             <div className="time">
                                 18 hrs ago
@@ -192,15 +190,10 @@ const SinglePost=(props)=>{
                                 <b>Post</b>
                             </div>
                         </div>
-                    
             </div>
        )
 }
     
-
-
-
-
 const AccountSection =()=>{
     return(
     <div className='accountSection' >
@@ -360,17 +353,17 @@ var postdata = [
         'innerpostlength':3,
         'post':[{
             'innerpostid':1,
-            'posttype':'video',
+            'type':'video',
             'mainpost':VideoPost
         },
         {
             'innerpostid':2,
-            'posttype':'umage',
+            'type':'image',
             'mainpost':GuitarOne
         },
         {
             'innerpostid':2,
-            'posttype':'video',
+            'type':'video',
             'mainpost':GuitarTwo
         }
         ],
@@ -395,33 +388,7 @@ var postdata = [
         'innerpostlength':1,
         'post':[{
             'innerpostid':1,
-            'posttype':'image',
-            'mainpost':PostImage1
-        }
-        ],
-        'postCaption':'Is this same for everyone ? ki ma matrai special case ho?',
-        'comments':[{
-            'id':1,
-            'username':'nepali_kaila',
-            'liked':false,
-            'comment':'haha testai hola ',
-        },
-        {
-            'id':2,
-            'username':'lonelybird',
-            'liked':false,
-            'comment':'ahh ni mero ma ni testai ho brudaaaaaa ',
-        }]
-    },
-
-    {
-        'postid':'asd1234567894',
-        'postowner':'memenepal',
-        'likedby':['onequotes','sanja01','jamal57','levchonu','priya'],
-        'innerpostlength':1,
-        'post':[{
-            'innerpostid':1,
-            'posttype':'image',
+            'type':'image',
             'mainpost':PostImage1
         }
         ],
@@ -446,22 +413,22 @@ var postdata = [
         'innerpostlength':4,
         'post':[{
             'innerpostid':1,
-            'posttype':'image',
+            'type':'image',
             'mainpost':PostImage3a
         },
         {
             'innerpostid':2,
-            'posttype':'image',
+            'type':'image',
             'mainpost':PostImage3b
         },
         {
             'innerpostid':3,
-            'posttype':'image',
+            'type':'image',
             'mainpost':PostImage3c
         },
         {
             'innerpostid':4,
-            'posttype':'image',
+            'type':'image',
             'mainpost':PostImage3d
         }
         ],
@@ -486,7 +453,7 @@ var postdata = [
         'innerpostlength':1,
         'post':[{
             'innerpostid':1,
-            'posttype':'image',
+            'type':'image',
             'mainpost': PostImage4a
         }
         ],
@@ -511,7 +478,7 @@ var postdata = [
         'innerpostlength':1,
         'post':[{
             'innerpostid':1,
-            'posttype':'video',
+            'type':'video',
             'mainpost': PostImage5a
         }
         ],
@@ -529,7 +496,6 @@ var postdata = [
             'comment':'Sir your hand moves vala video banakar apke YouTube ke Chanel par dalo na please || your fan Mayank',
         }]
     },
-    
 ]
 
                         
--- src/NavBar/SearchBox/searchresult.js
+++ src/NavBar/SearchBox/searchresult.js
@@ -187,7 +187,7 @@ const SearchHistoryList =(props)=>{
             </div>
         </div>
         <div  onClick={()=>{props.updatehistory(props.data.id)}}>
-        <svg aria-label="Close" class="_8-yf5 " fill="#8e8e8e" height="16" role="img" viewBox="0 0 48 48" width="16"><path clip-rule="evenodd" d="M41.8 9.8L27.5 24l14.2 14.2c.6.6.6 1.5 0 2.1l-1.4 1.4c-.6.6-1.5.6-2.1 0L24 27.5 9.8 41.8c-.6.6-1.5.6-2.1 0l-1.4-1.4c-.6-.6-.6-1.5 0-2.1L20.5 24 6.2 9.8c-.6-.6-.6-1.5 0-2.1l1.4-1.4c.6-.6 1.5-.6 2.1 0L24 20.5 38.3 6.2c.6-.6 1.5-.6 2.1 0l1.4 1.4c.6.6.6 1.6 0 2.2z" fill-rule="evenodd"></path></svg> 
+        <svg aria-label="Close"  fill="#8e8e8e" height="16" role="img" viewBox="0 0 48 48" width="16"><path clipRule="evenodd" d="M41.8 9.8L27.5 24l14.2 14.2c.6.6.6 1.5 0 2.1l-1.4 1.4c-.6.6-1.5.6-2.1 0L24 27.5 9.8 41.8c-.6.6-1.5.6-2.1 0l-1.4-1.4c-.6-.6-.6-1.5 0-2.1L20.5 24 6.2 9.8c-.6-.6-.6-1.5 0-2.1l1.4-1.4c.6-.6 1.5-.6 2.1 0L24 20.5 38.3 6.2c.6-.6 1.5-.6 2.1 0l1.4 1.4c.6.6.6 1.6 0 2.2z" fillRule="evenodd"></path></svg> 
         </div>
     </div>
     )
