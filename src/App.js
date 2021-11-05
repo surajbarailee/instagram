9938217142baaf55730d4773eedf539105b0b173
Suraj Baraili <baraileesuraj@gmail.com>
2021-11-05 14:05:57 +0545

    added login page

--- src/App.js
+++ src/App.js
@@ -4,33 +4,47 @@ import Body from './BodySection/Body'
 import Explore from './Explore/explore'
 import Profile from './Profile/Profile'
 import EditProfile from './Profile/Edit Profile/EditProfie';
-
-
 import {Route,Redirect,BrowserRouter as Router} from 'react-router-dom'
 import Inbox from './Inbox/Inbox';
 import NameTag from './Profile/NameTag/NameTag';
 import Login from './login/login';
+import {useLocation} from 'react-router-dom';
+import {useState} from 'react';
 function App() {
   return (
     <div style={{height:'100%',width:'100%'}}>
           <Router>
-            {/* <NavBar></NavBar>
-                <Route exact path="/" component={Body} />
-                <Route exact path="/home">
-                  <Redirect to="/" />
-                </Route>
-                <Route exact path="/explore/" component={Explore} />
-                <Route exact path="/inbox/" component={Inbox} />
-                <Route exact path="/suraj_barailee/" component={Profile} />
-                <Route exact path="/accounts/edit" component={EditProfile} />
-                <Route exact path="/nametag" component={NameTag} /> */}
-                {/* <Route exact path="/" component={Body} /> */}
-                {/* <Route exact path="/About" component={About} /> */}
-                <Login></Login>
+            <Route path='/login' component={Login}/>
+            <>
+              <DefaultContainer/>
+            </>
           </Router>
     </div>
 
   );
+}
+
+   
+
+const DefaultContainer = () => {
+  const location = useLocation();
+  const [locationvalue, setlocationvalue] = useState(location.pathname)
+  console.log(locationvalue)
+  return (
+    <>
+      {(window.location.pathname!=='/login')?<NavBar/>:''} 
+      <Route exact path="/" component={Body} />
+      <Route exact path="/home">
+        <Redirect to="/" />
+      </Route>
+      <Route exact path="/explore/" component={Explore} />
+      <Route exact path="/inbox/" component={Inbox} />
+      <Route exact path="/suraj_barailee/" component={Profile} />
+      <Route exact path="/accounts/edit" component={EditProfile} />
+      <Route exact path="/nametag" component={NameTag} />
+      
+    </>
+  )
 }
 
 export default App;
--- src/login/login.js
+++ src/login/login.js
@@ -1,90 +1,132 @@
 import './login.css';
 import PlayStoreImage from '../resources/playstore.png'
 import AppStoreImage from '../resources/appstore.png' 
-const Login=()=>{
+import LoginImage from '../resources/login.png'
+import { ProfileFooter } from '../Profile/Profile';
+import { Link } from 'react-router-dom';
+import screenshots1 from '../resources/screenshots/screenshots1.jpg'
+import screenshots2 from '../resources/screenshots/screenshots2.jpg'
+import screenshots3 from '../resources/screenshots/screenshots3.jpg'
+import screenshots4 from '../resources/screenshots/screenshots4.jpg'
+import screenshots5 from '../resources/screenshots/screenshots5.jpg'
 
+
+// create function to change the src of the image to the next image in the array 
+// every 3 seconds
+const changeImage = () => {
+    let images = [screenshots1, screenshots2, screenshots3, screenshots4, screenshots5]
+    let i = 0;
+    setInterval(() => {
+        if (i === images.length - 1) {
+            i = 0;
+        } else {
+            i++;
+        }
+        if(window.location.pathname==='/login'){
+            
+            document.getElementById("screenshots").className='screenshots'
+            document.getElementById('screenshots').src = images[i];
+            window.requestAnimationFrame(function(time) {
+                window.requestAnimationFrame(function(time) {
+                    document.querySelector(".screenshots").className = 'screenshots fade-in';
+                });
+            });
+        }
+        }, 3000);
+}
+
+window.addEventListener('load', (event) => {
+    changeImage()
+  });
+
+const Login=(props)=>{
     return (
-    <div className='loginWrapper'>
-        <div className="leftWrapper">
-
-        </div>
-        <div className="rightWrapper">
-            <div className="signIn">
-                <div className='signInlogoWrapper'>
-                    <div className="logo"/>
-                </div> 
-                <div className=" inputLoginWrapper username">
-                    <input className='inputLogin' type="text" placeholder='Phone number,username, or email'
-                        autoCapitalize='off'
-                        maxLength='75'
-                        name='username'
-                        autoCorrect='off'
-                    />
-                </div>
-                <div className=" inputLoginWrapper password">
-                    <input className='inputLogin' type="password" placeholder='Password'/>
-                </div>
-                <div className="login">
-                    <button className='loginButton' >
-                        Log In
-                    </button>
-                </div>
-                <div className="orWrapper">
-                    <div className='orLines'> 
-                    </div>
-                    <div>
-                        OR
-                    </div>
-                    <div className='orLines'>    
+        <div className="login-container">
+            <div className='loginWrapper'>
+                <div className="leftWrapper">
+                    <div className="imageWrapper">
+                        <img src={LoginImage} alt="Phone"/>
+                        <img src={screenshots1} alt="screenshot" className="screenshots fade-in" id="screenshots"/>
                     </div>
                 </div>
-                <div className="loginFacebook">
-                    <button className='loginFacebookButton'>
-                        <div className="facebookIcon">
-
+                <div className="rightWrapper">
+                    <div className="signIn">
+                        <div className='signInlogoWrapper'>
+                            <div className="logo"/>
+                        </div> 
+                        <div className=" inputLoginWrapper username">
+                            <input className='inputLogin' type="text" placeholder='Phone number,username, or email'
+                                autoCapitalize='off'
+                                maxLength='75'
+                                name='username'
+                                autoCorrect='off'
+                            />
                         </div>
-                        <div>
-                            Log in with Facebook
+                        <div className=" inputLoginWrapper password">
+                            <input className='inputLogin' type="password" placeholder='Password'/>
                         </div>
-                    </button>
-                </div>
-                <div className="forgotPassword">
-                    Forgot Password?
-                </div>
-            </div>
-            <div className="SignUpWrapper">
-                <div className="signUp">
+                        <div className="login">
+                            <Link className='loginButton' to='/'>
+                                Log In
+                            </Link>
+                        </div>
+                        <div className="orWrapper">
+                            <div className='orLines'> 
+                            </div>
+                            <div>
+                                OR
+                            </div>
+                            <div className='orLines'>    
+                            </div>
+                        </div>
+                        <div className="loginFacebook">
+                            <button className='loginFacebookButton'>
+                                <div className="facebookIcon">
 
-                    Dont have an account? <label className='signuplink'>Sign up</label>
+                                </div>
+                                <div>
+                                    Log in with Facebook
+                                </div>
+                            </button>
+                        </div>
+                        <div className="forgotPassword">
+                            Forgot Password?
+                        </div>
+                    </div>
+                    <div className="SignUpWrapper">
+                        <div className="signUp">
+
+                            Dont have an account? <label className='signuplink'>Sign up</label>
+                        </div>
+                        
+                    </div>
+                    <div className="getapp">
+                                <div className='getappText'>
+                                    Get the app.
+                                </div>
+                                <div className='storeLinks'>
+                            <div style={{paddingRight:'4px'}}>
+                                <a href="https://apps.apple.com/app/instagram/id389801252?vt=li">
+                                    <img src={AppStoreImage} alt="appstore link" width='136px'/>
+                                </a>
+                            </div>
+                            <div style={{paddingLeft:'4px'}}>
+                                <a href="https://play.google.com/store/apps/details?id=com.instagram.android">
+                                        <img src={PlayStoreImage} alt="playstore link" width='136px' />
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    
                 </div>
                 
+
+                
+                
             </div>
-            <div className="getapp">
-                        <div className='getappText'>
-                            Get the app.
-                        </div>
-                        <div className='storeLinks'>
-                    <div style={{paddingRight:'4px'}}>
-                        <a href="https://apps.apple.com/app/instagram/id389801252?vt=li">
-                            <img src={AppStoreImage} alt="appstore link" width='136px'/>
-                        </a>
-                    </div>
-                    <div style={{paddingLeft:'4px'}}>
-                        <a href="https://play.google.com/store/apps/details?id=com.instagram.android">
-                                <img src={PlayStoreImage} alt="playstore link" width='136px' />
-                        </a>
-                    </div>
-                </div>
-            </div>
-            
+            <ProfileFooter/>
         </div>
-        
-
-        
-        
-    </div>
     )
-
 }
 
 export default Login
